307c3d916bac5ca063ec4fc2cfb1148b5dcae383
Oshan Kottege <oshan.kottege@myob.com>
2018-09-12 08:28:53 +1200

    Added the arithmetic controller and it's 'Add' operation.

---
+++ Tests/Tracker.Api.Tests/ArithmeticControllerTests.cs
@@ -0,0 +1,21 @@
+﻿using TrackerService.Api.Controllers;
+using Xunit;
+
+namespace Tracker.Api.Tests
+{
+    public class ArithmeticControllerTests
+    {
+        private readonly ArithmeticController controller = new ArithmeticController();
+
+        [Theory]
+        [InlineData(1, 1, 2)]
+        [InlineData(0, 0, 0)]
+        [InlineData(5, -1, 4)]
+        [InlineData(-5, -1, -6)]
+        public void ShouldPerformBasicAddition(decimal a, decimal b, decimal expectedResult)
+        {
+            var result = controller.Add(a, b).Value;
+            Assert.Equal(expectedResult, result);
+        }
+    }
+}
---
+++ TrackerService.Api/Controllers/ArithmeticController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TrackerService.Api.Controllers
+{
+    [Route("api/arithmetic")]
+    public class ArithmeticController : ControllerBase
+    {
+        [Route("add/{a}/with/{b}")]
+        public ActionResult<decimal> Add(decimal a, decimal b)
+        {
+            return a + b;
+        }
+    }
+}
