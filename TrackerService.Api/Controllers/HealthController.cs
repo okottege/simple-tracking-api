eaf4b19688f7d5046a22e13f61c31569da73c42f
Oshan Kottege <oshan.kottege@gmail.com>
2018-09-12 22:21:43 +1200

    Added a new health endpoint and modified the deployment scripts.

---
+++ TrackerService.Api/Controllers/HealthController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Reflection;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TrackerService.Api.Controllers
+{
+    [Route("api/health")]
+    public class HealthController : ControllerBase
+    {
+        public ActionResult<object> Get()
+        {
+            return new
+            {
+                Version = Assembly.GetEntryAssembly().GetCustomAttribute<AssemblyInformationalVersionAttribute>()
+                    .InformationalVersion
+            };
+        }
+    }
+}
