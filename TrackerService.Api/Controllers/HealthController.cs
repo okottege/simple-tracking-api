14e8ba030aafff584fa513c1eaa5f3b5257672af
Oshan Kottege <oshan.kottege@myob.com>
2018-09-18 09:42:30 +1200

    Added logic to health controller to call the db.

--- TrackerService.Api/Controllers/HealthController.cs
+++ TrackerService.Api/Controllers/HealthController.cs
@@ -1,18 +1,31 @@
-﻿using System.Reflection;
+﻿using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using TrackerService.Data.Contracts;
 
 namespace TrackerService.Api.Controllers
 {
     [Route("api/health")]
     public class HealthController : ControllerBase
     {
-        public ActionResult<object> Get()
+        private readonly IEmployeeRepository employeeRepo;
+
+        public HealthController(IRepositoryFactory factory)
         {
+            employeeRepo = factory.CreateEmployeeRepository();
+        }
+
+        public async Task<ActionResult<object>> Get()
+        {
+            var employees = await employeeRepo.GetAllEmployees();
+
             return new
             {
                 Version = Assembly.GetEntryAssembly()
                                   .GetCustomAttribute<AssemblyInformationalVersionAttribute>()
-                                  .InformationalVersion
+                                  .InformationalVersion,
+                NumEmployees = employees.Count()
             };
         }
     }
