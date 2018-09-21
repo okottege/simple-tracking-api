3b01ab06fb0951afd6abd811982be53f71ce5e36
Oshan Kottege <oshan.kottege@gmail.com>
2018-09-21 23:28:52 +1200

    Added an integration test to check values endpoint.

--- Tracker.Api.IntegrationTests/ValuesEndpointTests.cs
+++ Tracker.Api.IntegrationTests/ValuesEndpointTests.cs
@@ -1,5 +1,7 @@
 using System;
+using System.Net.Http;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
 
 namespace Tracker.Api.IntegrationTests
 {
@@ -7,18 +9,27 @@ namespace Tracker.Api.IntegrationTests
     public class ValuesEndpointTests
     {
         private static TestContext context;
+        private static readonly HttpClientHandler httpClientHandler = new HttpClientHandler();
+        
+        private static HttpClient client;
 
         [ClassInitialize]
         public static void InitialiseTests(TestContext testContext)
         {
             context = testContext;
+            httpClientHandler.ServerCertificateCustomValidationCallback = (message, cert, chain, errors) => true;
+            client = new HttpClient(httpClientHandler);
         }
 
         [TestMethod]
         public void TestGet()
         {
             Console.WriteLine($"The web URL is: {context.Properties["webUrl"]}");
-            Assert.IsTrue(true);
+            client.BaseAddress = new Uri(context.Properties["webUrl"].ToString());
+
+            var result = client.GetAsync("api/values").Result;
+            var values = JsonConvert.DeserializeObject<string[]>(result.Content.ReadAsStringAsync().Result);
+            Assert.IsTrue(values.Length == 2);
         }
     }
 }
