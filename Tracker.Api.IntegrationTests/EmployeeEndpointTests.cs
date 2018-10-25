8b535b92069976fc1ed1fe92ecb3788f13c351f9
Oshan Kottege <oshan.kottege@myob.com>
2018-10-25 17:33:01 +1300

    Added an integration test for updating employee details.

--- Tracker.Api.IntegrationTests/EmployeeEndpointTests.cs
+++ Tracker.Api.IntegrationTests/EmployeeEndpointTests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Dynamic;
 using System.Linq;
 using System.Net;
@@ -14,10 +15,23 @@ namespace Tracker.Api.IntegrationTests
     [TestClass]
     public class EmployeeEndpointTests : BaseIntegrationTest
     {
+        private static readonly List<int> employeeIdsToRemove = new List<int>();
+            
         [ClassInitialize]
         public static async Task InitialiseTests(TestContext context)
         {
             await Initialise(context);
+        }
+
+        [ClassCleanup]
+        public static async Task RemoveCreatedEmployees()
+        {
+            foreach (var employeeId in employeeIdsToRemove)
+            {
+                await serviceClient.DeleteAsync($"api/employee/{employeeId}");
+            }
+
+            employeeIdsToRemove.Clear();
         }
 
         [TestMethod]
@@ -54,6 +68,33 @@ namespace Tracker.Api.IntegrationTests
             new[] {firstNameError, lastNameError, dateOfBirthError, startDateError}.ToList().ForEach(e => Assert.IsTrue(e.EndsWith("field is required.")));
         }
 
+        [TestMethod]
+        public async Task TestUpdateEmployeeDetails()
+        {
+            logger.WriteLine("Testing updating of an employee.");
+
+            dynamic employee = await CreateEmployeeThroughApi("Test", "Employee", new DateTime(1981, 1, 30), new DateTime(2007, 10, 12));
+            int employeeId = Convert.ToInt32(employee.EmployeeId.ToString());
+            Assert.IsTrue(employeeId > 0);
+
+            employee.DateOfBirth = new DateTime(2000, 1, 1);
+            employee.StartDate = new DateTime(2018, 10, 10);
+            var reqContent = new StringContent(JsonConvert.SerializeObject(employee));
+            reqContent.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+            var updateResponse = await serviceClient.PutAsync("api/employee", reqContent);
+
+            updateResponse.EnsureSuccessStatusCode();
+            Assert.AreEqual(HttpStatusCode.NoContent, updateResponse.StatusCode);
+
+            employeeIdsToRemove.Add(employeeId);
+
+            var employeeById = await GetEmployeeThroughApi(employee.EmployeeId);
+            Assert.AreEqual(employee.DateOfBirth.ToString(), employeeById.DateOfBirth.ToString());
+            Assert.AreEqual(employee.StartDate.ToString(), employeeById.StartDate.ToString());
+
+            logger.WriteLine("Updating of an employee is successful.");
+        }
+
         private async Task<int> TestEmployeeCreate(dynamic employee)
         {
             logger.WriteLine("Testing creation of a new employee");
@@ -78,9 +119,7 @@ namespace Tracker.Api.IntegrationTests
         {
             logger.WriteLine($"Test getting employee by Id: {employee.EmployeeId}");
 
-            var response = await serviceClient.GetAsync($"api/employee/{employee.EmployeeId}");
-            response.EnsureSuccessStatusCode();
-            var data = await GetResponsePayload(response);
+            var data = await GetEmployeeThroughApi(employee.EmployeeId);
 
             CheckEmployeeInfoWithPayload(employee, data);
             logger.WriteLine("Getting employee by id was successful.");
@@ -106,6 +145,37 @@ namespace Tracker.Api.IntegrationTests
             Assert.AreEqual(employee.DateOfBirth.ToString(), payload.DateOfBirth.ToString());
             Assert.AreEqual(employee.StartDate.ToString(), payload.StartDate.ToString());
             Assert.IsNotNull(payload.EmployeeId);
+        }
+
+        private dynamic GetNewEmployee(string firstName, string lastName, DateTime dob, DateTime startDate)
+        {
+            dynamic employee = new ExpandoObject();
+            employee.StartDate = startDate;
+            employee.DateOfBirth = dob;
+            employee.LastName = firstName;
+            employee.FirstName = lastName;
+
+            return employee;
+        }
+
+        private async Task<dynamic> CreateEmployeeThroughApi(string firstName, string lastName, DateTime dob, DateTime startDate)
+        {
+            dynamic employee = GetNewEmployee(firstName, lastName, dob, startDate);
+            var reqContent = new StringContent(JsonConvert.SerializeObject(employee));
+            reqContent.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+            var response = await serviceClient.PostAsync("api/employee", reqContent);
+            response.EnsureSuccessStatusCode();
+            dynamic data = await GetResponsePayload(response);
+
+            return data;
+        }
+
+        private async Task<dynamic> GetEmployeeThroughApi(dynamic employeeId)
+        {
+            var response = await serviceClient.GetAsync($"api/employee/{employeeId}");
+            response.EnsureSuccessStatusCode();
+            var data = await GetResponsePayload(response);
+            return data;
         }
     }
 }
