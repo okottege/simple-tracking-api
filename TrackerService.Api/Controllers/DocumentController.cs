0a0d799fcc70436b0877740266cb1eb9a6ef9687
Oshan Kottege <oshan.kottege@myob.com>
2018-10-30 09:35:53 +1300

    Got started working on the document upload.

---
+++ TrackerService.Api/Controllers/DocumentController.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http.Features;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.WebUtilities;
+using Microsoft.Net.Http.Headers;
+using TrackerService.Api.Filters;
+
+namespace TrackerService.Api.Controllers
+{
+    [Authorize]
+    [ApiController]
+    [Route("api/document")]
+    public class DocumentController : ControllerBase
+    {
+        private static readonly FormOptions defaultFormOptions = new FormOptions();
+
+        [HttpPost("upload")]
+        [DisableFormValueModelBinding]
+        public async Task<IActionResult> UploadAsync()
+        {
+            if (!IsMultipartContentType(Request.ContentType))
+            {
+                return BadRequest($"Expected a multipart request, but got {Request.ContentType}");
+            }
+
+            var formAccumulator = new KeyValueAccumulator();
+            string targetFilePath = null;
+
+            var boundary = GetBoundary(MediaTypeHeaderValue.Parse(Request.ContentType), defaultFormOptions.MultipartBoundaryLengthLimit);
+            var reader = new MultipartReader(boundary, Request.Body);
+            var section = await reader.ReadNextSectionAsync();
+
+            while (section != null)
+            {
+                ContentDispositionHeaderValue contentDisposition;
+                var hasContentDispositionHeader = ContentDispositionHeaderValue.TryParse(section.ContentDisposition, out contentDisposition);
+
+                if (hasContentDispositionHeader)
+                {
+                    if (HasFileContentDisposition(contentDisposition))
+                    {
+                        
+                    }
+                }
+            }
+
+            return Content("WIP");
+        }
+
+        private static string GetBoundary(MediaTypeHeaderValue contentType, int lengthLimit)
+        {
+            var boundary = HeaderUtilities.RemoveQuotes(contentType.Boundary);
+            if (!boundary.HasValue || string.IsNullOrWhiteSpace(boundary.Value))
+            {
+                throw new InvalidDataException("Missing content-type boundary.");
+            }
+
+            if (boundary.Length > lengthLimit)
+            {
+                throw new InvalidDataException($"Multipart boundary length limit {lengthLimit} exceeded");
+            }
+
+            return boundary.Value;
+        }
+
+        private static bool IsMultipartContentType(string contentType)
+        {
+            return !string.IsNullOrEmpty(contentType) &&
+                   contentType.IndexOf("multipart/", StringComparison.OrdinalIgnoreCase) >= 0;
+        }
+
+        private static bool HasFormDataContentDisposition(ContentDispositionHeaderValue contentDisposition)
+        {
+            return contentDisposition != null &&
+                   contentDisposition.DispositionType.Equals("form-data") &&
+                   string.IsNullOrEmpty(contentDisposition.FileName.Value) &&
+                   string.IsNullOrEmpty(contentDisposition.FileNameStar.Value);
+        }
+
+        public static bool HasFileContentDisposition(ContentDispositionHeaderValue contentDisposition)
+        {
+            return contentDisposition != null &&
+                   contentDisposition.DispositionType.Equals("form-data") &&
+                   (!string.IsNullOrEmpty(contentDisposition.FileName.Value) || !string.IsNullOrEmpty(contentDisposition.FileNameStar.Value));
+        }
+    }
+}
