1c4afd4bb97d74337aefbe87250cf4c7c02c25ed
Oshan Kottege <oshan.kottege@myob.com>
2018-11-09 16:47:34 +1300

    Trying to get the tests to work.

---
+++ Tests/Tracker.Api.Tests/Integration/Application/Authentication/TestStartup.cs
@@ -0,0 +1,89 @@
+﻿using System.IO;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json.Serialization;
+using TrackerService.Api;
+using TrackerService.Data;
+using TrackerService.Data.Contracts;
+
+namespace Tracker.Api.Tests.Integration.Application.Authentication
+{
+    public class TestStartup
+    {
+        private readonly IHostingEnvironment environment;
+
+        public TestStartup(IConfiguration configuration, IHostingEnvironment environment)
+        {
+            Configuration = configuration;
+            this.environment = environment;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            if (environment.IsDevelopment())
+            {
+                services.AddCors(options =>
+                {
+                    options.AddPolicy("DevPolicy", builder =>
+                    {
+                        builder.AllowAnyOrigin();
+                        builder.AllowAnyHeader();
+                        builder.AllowAnyMethod();
+                    });
+                });
+            }
+
+            services.AddAuthorization(options =>
+            {
+                options.DefaultPolicy = new AuthorizationPolicyBuilder(JwtBearerDefaults.AuthenticationScheme)
+                    .RequireAuthenticatedUser()
+                    .Build();
+            });
+
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(option =>
+            {
+                option.Authority = Configuration["Authentication:Authority"];
+                option.Audience = Configuration["Authentication:Audience"];
+
+            });
+            services.AddHttpClient();
+            var storageConn = new StorageConnectionInfo(Configuration.GetConnectionString("CloudStorage"), Configuration["StorageConnection:ContainerName"]);
+            services.AddTransient<IRepositoryFactory>(provider => new RepositoryFactory(Configuration.GetConnectionString("SimpleTaxDB"), storageConn));
+            services.AddMvc()
+                .AddJsonOptions(opt => opt.SerializerSettings.ContractResolver = new DefaultContractResolver())
+                .SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseCors("DevPolicy");
+            }
+            else
+            {
+                app.UseHsts();
+                app.UseHttpsRedirection();
+            }
+
+            app.UseAuthentication();
+            app.UseMvc();
+        }
+    }
+}
