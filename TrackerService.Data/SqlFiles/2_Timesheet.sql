cf7e59cac698fc69753752f7a7e8ddf13a2aecb4
Oshan Kottege <oshan.kottege@gmail.com>
2018-11-20 06:39:58 +1300

    Updated the timesheet sql script to see how sql scripts get combined.

--- TrackerService.Data/SqlFiles/2_Timesheet.sql
+++ TrackerService.Data/SqlFiles/2_Timesheet.sql
@@ -1,9 +1,16 @@
-﻿CREATE TABLE dbo.timesheet (
-    timesheetId INT IDENTITY (1, 1),
-    employeeId INT NOT NULL,
-    workDate DATETIME NOT NULL,
-    createdDate DATETIME NOT NULL,
-    modifiedDate DATETIME NOT NULL,
-    createdBy NVARCHAR(30) NOT NULL,
-    modifiedBy NVARCHAR(30) NOT NULL
-)
+﻿IF NOT EXISTS (SELECT * FROM sysobjects WHERE name='timesheet' AND xtype = 'U')
+BEGIN
+    CREATE TABLE dbo.timesheet (
+        timesheetId INT IDENTITY (1, 1),
+        employeeId INT NOT NULL,
+        workDate DATETIME NOT NULL,
+        createdDate DATETIME NOT NULL,
+        createdBy NVARCHAR(30) NOT NULL,
+        modifiedDate DATETIME NOT NULL,        
+        modifiedBy NVARCHAR(30) NOT NULL,
+
+        CONSTRAINT PK_timesheet_timesheetId PRIMARY KEY (timesheetId),
+        CONSTRAINT FK_employee_timesheet_employeeId FOREIGN KEY (employeeId) REFERENCES employee (employeeId)
+    )
+END
+GO
