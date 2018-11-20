68d06c7b28438c2af193f09ade6ef4dfeb4dd77e
Oshan Kottege <oshan.kottege@gmail.com>
2018-11-20 21:34:26 +1300

    Added the timesheet entry table.

--- TrackerService.Data/SqlFiles/2_Timesheet.sql
+++ TrackerService.Data/SqlFiles/2_Timesheet.sql
@@ -14,3 +14,18 @@ BEGIN
     )
 END
 GO
+
+IF NOT EXISTS (SELECT * FROM sysobjects WHERE name='timesheetEntry' AND xtype = 'U')
+BEGIN
+    CREATE TABLE dbo.timesheetEntry (
+        timesheetEntryId BIGINT IDENTITY (1, 1),
+        timesheetId INT NOT NULL,
+        startDate DATETIME NOT NULL,
+        endDate DATETIME NOT NULL,
+        notes NVARCHAR (1000),
+
+        CONSTRAINT PK_timesheetEntry_timesheetEntryId PRIMARY KEY (timesheetEntryId),
+        CONSTRAINT FK_timesheet_timesheetEntry_timesheetId FOREIGN KEY (timesheetId) REFERENCES timesheet (timesheetId)
+    )
+END
+GO
