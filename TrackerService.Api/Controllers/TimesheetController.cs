ff08212d91a6ba45cbd6d3543017df302702c5be
Oshan Kottege <oshan.kottege@gmail.com>
2018-11-22 08:45:57 +1300

    Added the timesheet controller.

---
+++ TrackerService.Api/Controllers/TimesheetController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TrackerService.Data.Contracts;
+using TrackerService.Data.DataObjects;
+
+namespace TrackerService.Api.Controllers
+{
+    [ApiController]
+    [Route("api/timesheets")]
+    public class TimesheetController : ControllerBase
+    {
+        private readonly ITimesheetRepository timesheetRepo;
+
+        public TimesheetController(IRepositoryFactory factory)
+        {
+            timesheetRepo = factory.CreateTimesheetRepository();
+        }
+
+        [HttpGet]
+        [ProducesResponseType(200, Type = typeof(IEnumerable<Timesheet>))]
+        public async Task<IActionResult> GetAllTimesheets()
+        {
+            var timesheets = await timesheetRepo.GetTimesheets();
+            return Ok(timesheets);
+        }
+    }
+}
