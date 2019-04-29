029a5a0fbc0e44fc8aae9b9b304001554ffdbbf1
Oshan Kottege <Oshan.Kottege@myob.com>
2019-04-29 17:26:49 +1200

    Added the simple error endpoint.

--- TrackerService.Api/Controllers/ValuesController.cs
+++ TrackerService.Api/Controllers/ValuesController.cs
@@ -30,22 +30,10 @@ namespace TrackerService.Api.Controllers
             return "value";
         }
 
-        // POST api/values
-        [HttpPost]
-        public void Post([FromBody] string value)
+        [Route("error")]
+        public IActionResult Error()
         {
-        }
-
-        // PUT api/values/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
-        }
-
-        // DELETE api/values/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
+            throw new ApplicationException("This is a test.");
         }
     }
 }
