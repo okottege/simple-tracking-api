3005576f2c66cb6057b9017bfd72656fe975a2e3
Oshan Kottege <oshan.kottege@gmail.com>
2019-05-17 23:43:56 +1200

    Added logic to push the web app image upto the acr.

--- TrackerService.Api/deployment/web-image-create.ps1
+++ TrackerService.Api/deployment/web-image-create.ps1
@@ -2,7 +2,22 @@ param(
     [string] $webAppVersion = "v1.1",
     [string] $imageName = "tracking-svc-api"
 )
+
+Write-Host "Script root is: $PSScriptRoot"
+. $PSScriptRoot\db-vars.ps1
+
 Write-Host "Building an image for image: $imageName with version: $webAppVersion"
-Write-Host "Current working directory is: ${pwd}"
-docker build -f TrackerService.Api/multi-stage.DOCKERFILE -t "${imageName}:$webAppVersion" .
+docker build -f $PSScriptRoot/../multi-stage.DOCKERFILE -t "${imageName}:$webAppVersion" .
 Write-Host "Successfully built the image."
+
+Write-Host "Loging in to the acr"
+az acr login --name $acrName
+
+$acrLoginServer = az acr show --name $acrName --query loginServer --output tsv
+
+Write-Host "Tagging web app image"
+$webAppImageFullName = "$acrLoginServer/${imageName}:$webAppVersion"
+docker tag ${imageName}:$webAppVersion $webAppImageFullName
+
+Write-Host "Pushing the image: '$webAppImageFullName' to ACR"
+docker push $webAppImageFullName
