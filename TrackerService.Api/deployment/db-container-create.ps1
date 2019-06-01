ec0e5196100ed8fb4edd367eb0bf7e4d5d02beeb
Oshan Kottege <oshan.kottege@gmail.com>
2019-06-01 20:45:10 +1200

    Trying to see why the variable is not set at relealse time.

--- TrackerService.Api/deployment/db-container-create.ps1
+++ TrackerService.Api/deployment/db-container-create.ps1
@@ -15,17 +15,17 @@ az login --service-principal -u $azureLoginId -p $azureLoginSecret -t $azureTena
 Write-Host "Creating the Azure Container Instance with MSSQL 2017" -ForegroundColor Green
 Write-Host "Creating the resource group for ACI"
 $rgGuid = New-Guid
-$rgForAci = "${rgForAci}_${rgGuid}"
-Write-Host "Writing the resource group name to VSO variable with new value: $rgForAci"
-Write-Host "##vso[task.setvariable variable=IntTestDbAciRsourceGroup]$rgForAci"
-az group create --name $rgForAci --location $location
+$newRgNameForAci = "${rgForAci}_${rgGuid}"
+Write-Host "Writing the resource group name to VSO variable with new value: $newRgNameForAci"
+Write-Host "##vso[task.setvariable variable=IntTestDbAciRsourceGroup]$newRgNameForAci"
+az group create --name $newRgNameForAci --location $location
 
 Write-Host "Getting acr login server"
 $acrLoginServer = (az acr show --name $acrName --query loginServer --output tsv)
 
 $containerGroupName = "ms-sql-2017-${rgGuid}"
 Write-Host "Creating the container: $containerGroupName"
-az container create --resource-group $rgForAci `
+az container create --resource-group $newRgNameForAci `
     --name $containerGroupName `
     --image $acrLoginServer/ms-sql-2017:v1 `
     --cpu 1 --memory 3.5 `
