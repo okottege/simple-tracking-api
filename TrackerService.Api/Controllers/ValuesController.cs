efeef4a29b91f4ae5d72cb6bf0272a4d9c2ba951
Oshan Kottege <oshan.kottege@gmail.com>
2019-06-22 21:15:44 +1200

    Got rid of the config settings endpoint.

--- TrackerService.Api/Controllers/ValuesController.cs
+++ TrackerService.Api/Controllers/ValuesController.cs
@@ -50,11 +50,5 @@ namespace TrackerService.Api.Controllers
         {
             return BadRequest("Simple bad request response");
         }
-
-        [HttpGet("config-settings")]
-        public IActionResult ConfigSettings()
-        {
-            return Ok(new {ConnectionString = config["ConnectionStrings:SimpleTaxDB"]});
-        }
     }
 }
