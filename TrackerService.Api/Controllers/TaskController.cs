fe0c8f5db2e9a4d528019f29cf261f6fa4e23e99
Oshan Kottege <oshan.kottege@gmail.com>
2019-08-05 21:17:03 +1200

    Started work on the task controller.

---
+++ TrackerService.Api/Controllers/TaskController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading.Tasks;
+using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using TrackerService.Api.ViewModels.Tasks;
+using TrackerService.Core.Repositories;
+
+namespace TrackerService.Api.Controllers
+{
+    [Authorize]
+    [ApiController]
+    [Route("api/tasks")]
+    public class TaskController : ControllerBase
+    {
+        private readonly ITaskRepository taskRepo;
+        private readonly IMapper mapper;
+
+        public TaskController(ITaskRepository taskRepo, IMapper mapper)
+        {
+            this.taskRepo = taskRepo;
+            this.mapper = mapper;
+        }
+
+        [HttpPost]
+        public Task<IActionResult> CreateTask(CreateTaskViewModel model)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ TrackerService.Api/ViewModels/Tasks/CreateTaskViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using TrackerService.Core.CoreDomain.Tasks;
+
+namespace TrackerService.Api.ViewModels.Tasks
+{
+    public class CreateTaskViewModel
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public TaskType Type { get; set; }
+        public string ParentTaskId { get; set; }
+        public DateTime? DueDate { get; set; }
+    }
+}
